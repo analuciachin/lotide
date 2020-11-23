3d99f12cbce457b3f99901fcc5b682ca568a2bd1
analuciachin <ananime@gmail.com>
2020-11-23 22:44:14 +0000

    implemented head

--- head.js
+++ head.js
@@ -7,8 +7,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 2);
+const head = function (arr) {
+  return arr[0]
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([2]), 2);
+assertEqual(head([]), undefined);
